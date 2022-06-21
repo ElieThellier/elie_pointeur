26081147636a811b5f296022b483a1f741b373c7
ElieThellier <eliethellier@gmail.com>
2022-06-21 11:28:49 +0200

    ça marche, j'ai copié ton code

---
+++ pointeur.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include"pointeur.h"
+
+int main(){
+    printf("%d\n",len("pomme"));
+    printf("%d\n",len("Melenchon"));
+    
+    return 0;
+}
+
+int len(char* s){
+    char* carac = s;
+    int compteur = 0;
+    while (*carac != '\0'){
+        compteur++;
+        carac=carac+1;
+    }
+    return compteur;
+}
